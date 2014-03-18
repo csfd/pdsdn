a4412bbb6ef058a5b12f8015d5c85ad158bf5c1e
Shi Chen <victorchen2518@gmail.com>
2014-03-18 23:43:08 +0800

    add test framework

---
+++ src/main/java/com/csfd/pdsdn/policy/json/JsonPolicyParser.java
@@ -0,0 +1,6 @@
+package com.csfd.pdsdn.policy.json;
+
+import org.json.JSONObject;
+
+public class JsonPolicyParser {
+}
---
+++ src/main/java/com/csfd/pdsdn/policy/json/jsonLoader.java
@@ -0,0 +1,22 @@
+package com.csfd.pdsdn.policy.json;
+
+import org.json.JSONObject;
+
+public class jsonLoader {
+   private JSONObject jsonObject;
+
+   /**
+    * @return the jsonObject
+    */
+   public JSONObject getJsonObject() {
+      return jsonObject;
+   }
+
+   /**
+    * @param jsonObject
+    *           the jsonObject to set
+    */
+   public void setJsonObject(JSONObject jsonObject) {
+      this.jsonObject = jsonObject;
+   }
+}
